888f07bfe5d4694cf089183aa5474a770b852fa8
serd2011 <45919738+serd2011@users.noreply.github.com>
2020-07-01 00:00:39 +0300

    Добавлена панель настроек

--- app/js/index.js
+++ app/js/index.js
@@ -6,7 +6,54 @@ let additional_lessons_but;
 let additional_inf;
 let additional_lessons;
 
+let settings = {};
+
 $(document).ready(ready);
+
+$.ajax({
+	url: "https://api.guap.ru/rasp/custom/get-sem-info",
+}).done(function (data) {
+	console.log(data);
+}).fail(function () {
+	console.log("error");
+});
+
+function ready() {
+	page_prepair();
+	chrome.storage.sync.get(null, function () {
+
+	});
+	$("body").addClass("shown");
+}
+
+function page_prepair() {
+	article = document.getElementsByTagName("article")[0];
+	day_switch = document.getElementById("checkbox");
+	additional_lessons_but = document.getElementsByClassName("but_additional_lessons")[0];
+	additional_inf = document.getElementsByClassName("additional_inf")[0];
+	additional_lessons = document.getElementsByClassName("additional_lessons")[0];
+
+
+	day_switch.addEventListener("change", day_change, true);
+
+	additional_lessons_but.addEventListener("click", additional_lessons_but_click, true);
+
+	$("#settings-button-open").click(settings_open);
+	$("#settings-button-close").click(settings_close);
+	$("#settings-background").click(settings_close);
+
+	$("#settings-block *[data-role='control']").each(function () {
+		$(this).change(setting_changed);
+	});
+
+	chrome.storage.sync.get(["theme"], function (result) {
+		switch (result.theme) {
+			case "dark":
+				$("body").addClass("dark");
+				break;
+		}
+	});
+}
 
 function day_change() {
 	if (day_switch.checked) {
@@ -28,36 +75,17 @@ function additional_lessons_but_click() {
 	}
 }
 
-$.ajax({
-	url: "https://api.guap.ru/rasp/custom/get-sem-info",
-}).done(function (data) {
-	console.log(data);
-}).fail(function () {
-	console.log("error");
-});
-
-function ready() {
-
-	article = document.getElementsByTagName("article")[0];
-	day_switch = document.getElementById("checkbox");
-	additional_lessons_but = document.getElementsByClassName("but_additional_lessons")[0];
-	additional_inf = document.getElementsByClassName("additional_inf")[0];
-	additional_lessons = document.getElementsByClassName("additional_lessons")[0];
-
-
-	day_switch.addEventListener("change", day_change, true);
-	additional_lessons_but.addEventListener("click", additional_lessons_but_click, true);
-
-
-	chrome.storage.sync.get(["theme"], function (result) {
-		switch (result.theme) {
-			case 1:
-				$("body").addClass("dark");
-				break;
-		}
-	});
-
-	$("body").addClass("shown");
+function settings_open() {
+	$("#settings-block").addClass("open");
+	$("#settings-background").addClass("shown");
 }
 
-// chrome.storage.sync.set({ 'theme': 0 });
+function settings_close() {
+	$("#settings-block").removeClass("open");
+	$("#settings-background").removeClass("shown");
+}
+
+function setting_changed() {
+	if ($(this).data("type") == "sync")
+		chrome.storage.sync.set({ [$(this).data("name")]: $(this).val() });
+}
