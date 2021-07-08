53b44bcd61b40976802fd07b9174a9f2a375af08
serd2011 <45919738+serd2011@users.noreply.github.com>
2021-07-08 23:37:48 +0300

    Исправлены описание и скрипты запуска

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -6,31 +6,33 @@ const baseConfig = require("./webpack.config.base.js");
 
 const debugCSP = { extension_pages: "script-src 'self' http://localhost:8097; object-src 'self';" };
 
-module.exports = webpackMerge.merge(baseConfig, {
-    mode: "development",
-    devtool: "inline-source-map",
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "src/index.html",
-            dev: true,
-            minify: {
-                collapseWhitespace: true
-            }
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: "./chrome", to: "./../" },
-                {
-                    from: "./chrome/manifest.json", to: "./../",
-                    transform: function (content, path) {
-                        json = JSON.parse(content.toString());
-                        json["content_security_policy"] = debugCSP;
-                        return JSON.stringify(json);
-                    },
-                    force: true,
-                    priority: 1
+module.exports = (env) => {
+    return webpackMerge.merge(baseConfig, {
+        mode: "development",
+        devtool: "inline-source-map",
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "src/index.html",
+                dev: env.devtools,
+                minify: {
+                    collapseWhitespace: true
                 }
-            ]
-        })
-    ]
-});
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: "./chrome", to: "./../" },
+                    {
+                        from: "./chrome/manifest.json", to: "./../",
+                        transform: function (content, path) {
+                            json = JSON.parse(content.toString());
+                            json["content_security_policy"] = debugCSP;
+                            return JSON.stringify(json);
+                        },
+                        force: true,
+                        priority: 1
+                    }
+                ]
+            })
+        ]
+    });
+}
