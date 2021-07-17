e7278e12e2c458cbc7d71c2e94f2c133dd3f0466
serd2011 <45919738+serd2011@users.noreply.github.com>
2021-07-17 05:29:41 +0300

    fix: backgound.js Исправлено поведение при наличии нескольких окон браузера.

--- chrome/background.js
+++ chrome/background.js
@@ -1,26 +1,23 @@
-
-//id of current opened app tab 
-let tab_id = chrome.tabs.TAB_ID_NONE;
-
-//call openTimetable on click on browserAction
 chrome.action.onClicked.addListener(openApp);
 
-//clear tab_id if app tab is closed
-chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
-	if (tab_id == tabId)
-		tab_id = chrome.tabs.TAB_ID_NONE;
-});
-
-//shortcuts command
 chrome.commands.onCommand.addListener(function (command) {
 	if (command = "openApp")
 		openApp();
 });
 
 //open new or focus to app tab
-function openApp() {
-	if (tab_id == chrome.tabs.TAB_ID_NONE)
-		chrome.tabs.create({ url: chrome.runtime.getURL("app/index.html") }, function (tab) { tab_id = tab.id; });
-	else
-		chrome.tabs.update(tab_id, { active: true });
+async function openApp() {
+	let appUrl = chrome.runtime.getURL("app/index.html");
+	let tabs = await chrome.tabs.query({ url: appUrl });
+
+	if (tabs.length === 0)
+		return chrome.tabs.create({ url: appUrl }, function (tab) { tab_id = tab.id; });
+
+	let currentWindow = await chrome.windows.getCurrent();
+	for (let tab of tabs) {
+		if (tab.windowId == currentWindow.id)
+			return chrome.tabs.update(tab.id, { active: true });
+	}
+	chrome.tabs.update(tabs[0].id, { active: true });
+	chrome.windows.update(tabs[0].windowId, { focused: true });
 }
